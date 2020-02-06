567ab367d31ed1c6591fe70451dda2f8a5429814
megadose <51822495+megadose@users.noreply.github.com>
2020-02-06 08:42:24 +0100

    Update toutatis.py

--- toutatis.py
+++ toutatis.py
@@ -5,11 +5,12 @@ import unicodecsv as csv
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser._action_groups.pop()
 required = parser.add_argument_group('required arguments')
 parser.add_argument('-s', '--sessionid',help="Instagram session ID",required=True)
-parser.add_argument('-u', '--usernames',help="List of usernames (filename)",required=True)
-parser.add_argument('-o', '--output',help="Name of output csv file",default="output_toutatis.csv")
+parser.add_argument('-i','--informations',help="The informations you want. mail : just extract the mail, phone : just extract the phone number ,ep extract email and phone , all : extract all informations")
+parser.add_argument('-f', '--file',help="List of usernames (filename)")
+parser.add_argument('-u','--username',help="One username")
+parser.add_argument('-o', '--output',help="Name of output csv file")
 args = parser.parse_args()
 sessionsId=args.sessionid
 ListOfInfo = []
@@ -28,20 +29,94 @@ def getInfo(userId,sessionId):
     infoUser = info["user"]
     return(infoUser)
 
-with open(args.usernames) as file:
-    usernames = [l.strip() for l in file]
-print(str(len(usernames))+" users")
-print("Export filename : "+args.output)
-for username in tqdm(usernames):
-    userid = getUserId(str(username))
-    ListOfInfo.append(getInfo(userid,sessionsId))
+def exportDictUnicode(dictToExport,exportFilename):
+    keys = []
+    for listinfo in dictToExport:
+    	for i in listinfo.keys():
+    		keys.append(i) if i not in keys else keys
 
-keys = []
-for listinfo in ListOfInfo:
-	for i in listinfo.keys():
-		keys.append(i) if i not in keys else keys
+    with open(exportFilename, 'wb') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(dictToExport)
 
-with open(args.output, 'wb') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(ListOfInfo)
+def extractEmail(dict):
+    try:
+        return(dict["public_email"])
+    except:
+        return("NULL")
+def extractPhone(dict):
+    try:
+        return(dict["public_phone_country_code"]+dict["public_phone_number"])
+    except:
+        return("NULL")
+
+if(args.username !=  None):
+    print("The informations about : "+args.username)
+    userid = getUserId(str(args.username))
+    print("User id : "+userid)
+    info = getInfo(userid,sessionsId)
+    if(args.informations == "mail"):
+        email = extractEmail(info)
+        print("Mail of {} : {}".format(args.username,email))
+        dict = {'username':username,'mail':email}
+        ListOfInfo.append(dict)
+    elif(args.informations == "phone"):
+        phone = extractPhone(info)
+        print("Phone of {} : {}".format(args.username))
+        dict = {'username':username,'phone':phone}
+        ListOfInfo.append(dict)
+
+    elif(args.informations == "all"):
+        ListOfInfo.append(info)
+    else:
+        print("check --help")
+        exit()
+    if (args.output != None):
+        print(ListOfInfo)
+        exportDictUnicode(ListOfInfo,args.output)
+        print("Export in "+args.output)
+elif(args.file != None):
+    if (args.output != None):
+        try:
+            with open(args.file) as f:
+                usernames = [l.strip() for l in f]
+                # Do something with the file
+        except IOError:
+            print("File not accessible")
+        print(str(len(usernames))+" users")
+
+        if(args.informations == "mail"):
+            for username in tqdm(usernames):
+                userid = getUserId(str(username))
+                info = getInfo(userid,sessionsId)
+                email = extractEmail(info)
+                dict = {'username':username,'mail':email}
+                ListOfInfo.append(dict)
+
+        elif(args.informations == "phone"):
+            for username in tqdm(usernames):
+                userid = getUserId(str(username))
+                info = getInfo(userid,sessionsId)
+                phone = extractPhone(info)
+                dict = {'username':username,'phone':phone}
+                ListOfInfo.append(dict)
+        elif(args.informations == "all"):
+            for username in tqdm(usernames):
+                userid = getUserId(str(username))
+                info = getInfo(userid,sessionsId)
+                ListOfInfo.append(info)
+        else:
+            print("check --help")
+            exit()
+
+        #print(ListOfInfo)
+        exportDictUnicode(ListOfInfo,args.output)
+        print("Export in "+args.output)
+
+    else:
+        print("Export a required if you want get informations of multiple usernames")
+
+else:
+    print("check --help")
+    exit()
