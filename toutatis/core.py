f63a8768a92f1d31c57e6e9956869c4aa6e8bfcd
megadose <51822495+megadose@users.noreply.github.com>
2020-05-28 19:02:52 +0200

    Create core.py

---
+++ toutatis/core.py
@@ -0,0 +1,67 @@
+import argparse,json
+from requests import get
+from quidam import instagram
+
+def recoveryEmail(username):
+    return(instagram(username))
+
+def getUserId(username,sessionsId):
+    r = get('https://www.instagram.com/{}/?__a=1'.format(username))
+    info = json.loads(r.text)
+    id = info["logging_page_id"].strip("profilePage_")
+    return(id)
+
+def getInfo(username,sessionId):
+    userId = getUserId(username,sessionId)
+    cookies = {'sessionid': sessionId}
+    headers = {'User-Agent': 'Instagram 64.0.0.14.96',}
+    response = get('https://i.instagram.com/api/v1/users/'+userId+'/info/', headers=headers, cookies=cookies)
+    info = json.loads(response.text)
+    infoUser = info["user"]
+    return(infoUser)
+
+def getFullName(username,sessionId):
+    infos = getInfo(username,sessionId)
+    return(infos["full_name"])
+
+def getProfilePicture(username,sessionId):
+    infos = getInfo(username,sessionId)
+    return(infos["profile_pic_url"])
+
+def getBiographie(username,sessionId):
+    infos = getInfo(username,sessionId)
+    return(infos["biography"])
+
+def extractEmail(username,sessionId):
+    userId = getUserId(username,sessionId)
+    dict = getInfo(userId,sessionId)
+    try:
+        return(dict["public_email"])
+    except:
+        return("NULL")
+
+def extractPhone(username,sessionId):
+    userId = getUserId(username,sessionId)
+    dict = getInfo(userId,sessionId)
+    try:
+        return(dict["public_phone_country_code"]+dict["public_phone_number"])
+    except:
+        return("NULL")
+
+def getAllInfos(username,sessionId):
+    userId=getUserId(username,sessionId)
+    recoveryemail=recoveryEmail(username)
+    cookies = {'sessionid': sessionId}
+    headers = {'User-Agent': 'Instagram 64.0.0.14.96',}
+    response = get('https://i.instagram.com/api/v1/users/'+userId+'/info/', headers=headers, cookies=cookies)
+    info = json.loads(response.text)
+    infos = info["user"]
+    try:
+        publicEmail=infos["public_email"]
+    except:
+        publicEmail=""
+    try:
+        publicPhone=str(infos["public_phone_country_code"]+infos["public_phone_number"])
+    except:
+        publicPhone=""
+    return({"username":username,"userID":userId,"FullName":infos["full_name"],"biography":str(infos["biography"]),"publicEmail":publicEmail,"public_phone_number":publicPhone,"recoveryEmail":recoveryemail,"ProfilePicture":infos["profile_pic_url"]})
