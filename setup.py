d2d3df6a5b516e2e231d8b6dadbbbf98e4e47c36
megadose <51822495+megadose@users.noreply.github.com>
2020-06-30 20:28:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='toutatis',
-    version="1.2",
+    version="1.21",
     packages=find_packages(),
     author="megadose",
     install_requires=["quidam"],
