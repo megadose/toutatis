ac0e9f4302c1df7de9f4bf4455e9f2f244d1dab1
Palenath <megadose@protonmail.com>
2021-04-21 15:56:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='toutatis',
-    version="1.23",
+    version="1.24",
     packages=find_packages(),
     author="megadose",
     install_requires=["argparse","tabulate","httpx","tqdm","unicodecsv"],
