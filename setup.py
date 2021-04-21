0812cbc61bc65fbde3dc1faddc34103bb5bfe8a5
Palenath <megadose@protonmail.com>
2021-04-21 15:52:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="It is a tool written to retrieve private information such as Phone Number, Mail Address, ID on Instagram accounts via API.",
     long_description="It is a tool written to retrieve private information such as Phone Number, Mail Address, ID on Instagram accounts via API.",
     include_package_data=True,
-    url='http://github.com/eyupergin/toutatis',
+    url='http://github.com/megadose/toutatis',
     entry_points = {'console_scripts': ['toutatis = toutatis.core:main']},
     classifiers=[
         "Programming Language :: Python",
