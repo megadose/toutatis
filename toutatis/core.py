5267eadf903693298bd0bc49d5f423f36898b6f5
аэт <85891169+novitae@users.noreply.github.com>
2022-04-26 13:19:00 +0200

    Update core.py
    - Removed httpx, unused or wrongly used, and using requests in the same time. Now using only requests.
    - Removed a lot of unused imports
    - Added a clear splitted error of User not found and rate limit in getUserId(). When 404, user not found.
    - Cleaned getInfo()
    - Changed the advanced_lookup(), removing encoding, not using the key anymore, if you reach rate limit, it will be from the ip and not from the key now
    - Indentation of the description when there's break-ins in it

--- toutatis/core.py
+++ toutatis/core.py
@@ -1,11 +1,7 @@
-import sys
-import colorama
-import argparse,json
-import httpx,hmac,hashlib,urllib
+import argparse
 import requests
-import os
-from httpx import get
-from colorama import Fore, Back, Style, init
+from urllib.parse import quote_plus
+from json import dumps, decoder
 
 import phonenumbers
 from phonenumbers.phonenumberutil import (
@@ -14,128 +10,134 @@ from phonenumbers.phonenumberutil import (
 )
 import pycountry
 
-colorama.init(autoreset=True)
-
-
 def getUserId(username,sessionsId):
     cookies = {'sessionid': sessionsId}
-    headers = {'User-Agent': 'Instagram 64.0.0.14.96',}
-    r = get('https://www.instagram.com/{}/?__a=1'.format(username),headers=headers, cookies=cookies)
+    headers = {'User-Agent': 'Instagram 64.0.0.14.96'}
+    api = requests.get(
+        f'https://www.instagram.com/{username}/?__a=1',
+        headers=headers,
+        cookies=cookies
+    )
     try:
-        info = json.loads(r.text)
-        id = info["logging_page_id"].strip("profilePage_")
-        return({"id":id,"error":None})
-    except :
-        return({"id":None,"error":"User not found or rate limit"})
+        if api.status_code == 404:
+            return {"id": None, "error": "User not found"}
+        
+        id = api.json()["logging_page_id"].strip("profilePage_")
+        return {"id":id, "error": None}
+
+    except decoder.JSONDecodeError:
+        return {"id":None, "error":"Rate limit"}
 
 def getInfo(username,sessionId):
-    userId=getUserId(username,sessionId)
-    if userId["error"]!=None:
-        return({"user":None,"error":"User not found or rate limit"})
-    else:
-        cookies = {'sessionid': sessionId}
-        headers = {'User-Agent': 'Instagram 64.0.0.14.96',}
-        response = get('https://i.instagram.com/api/v1/users/'+userId["id"]+'/info/', headers=headers, cookies=cookies)
-        info = json.loads(response.text)
-        infoUser = info["user"]
-        infoUser["userID"]=userId["id"]
-        return({"user":infoUser,"error":None})
+    userId = getUserId(username, sessionId)
+    if userId["error"]:
+        return userId
 
+    response = requests.get(
+        f'https://i.instagram.com/api/v1/users/{userId["id"]}/info/',
+        headers={'User-Agent': 'Instagram 64.0.0.14.96'},
+        cookies={'sessionid': sessionId}
+    ).json()["user"]
+    
+    infoUser = response
+    infoUser["userID"] = userId["id"]
+    
+    return {"user":infoUser, "error":None}
 
 def advanced_lookup(username):
-    USERS_LOOKUP_URL = 'https://i.instagram.com/api/v1/users/lookup/'
-    SIG_KEY_VERSION = '4'
-    IG_SIG_KEY = 'e6358aeede676184b9fe702b30f4fd35e71744605e39d2181a34cede076b3c33'
+    """
+        Post to get obfuscated login infos
+    """
+    data = "signed_body=SIGNATURE."+quote_plus(dumps(
+        {"q":username, "skip_recovery":"1"},
+        separators=(",",":")
+    ))
+    api = requests.post(
+        'https://i.instagram.com/api/v1/users/lookup/',
+        headers={
+            "Accept-Language": "en-US",
+            "User-Agent": "Instagram 101.0.0.15.120",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-IG-App-ID": "124024574287414",
+            "Accept-Encoding": "gzip, deflate",
+            "Host": "i.instagram.com",
+            #"X-FB-HTTP-Engine": "Liger",
+            "Connection": "keep-alive",
+            "Content-Length": str(len(data))
+        },
+        data=data
+    )
 
-    def generate_signature(data):
-        return 'ig_sig_key_version=' + SIG_KEY_VERSION + '&signed_body=' + hmac.new(IG_SIG_KEY.encode('utf-8'),data.encode('utf-8'),hashlib.sha256).hexdigest() + '.'+ urllib.parse.quote_plus(data)
-
-    def generate_data( phone_number_raw):
-        data = {'login_attempt_count': '0',
-                'directly_sign_in': 'true',
-                'source': 'default',
-                'q': phone_number_raw,
-                'ig_sig_key_version': SIG_KEY_VERSION
-                }
-        return data
-
-    data=generate_signature(json.dumps(generate_data(username)))
-    headers={
-    "Accept-Language": "en-US",
-    "User-Agent": "Instagram 101.0.0.15.120",
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Accept-Encoding": "gzip, deflate",
-    "X-FB-HTTP-Engine": "Liger",
-    "Connection": "close"}
     try:
-        r = httpx.post(USERS_LOOKUP_URL,headers=headers,data=data)
-        rep=r.json()
-        return({"user":rep,"error":None})
-    except :
-        return({"user":None,"error":"rate limit"})
-
+        return({"user": api.json(),"error": None})
+    except decoder.JSONDecodeError:
+        return({"user": None, "error": "rate limit"})
 
 def main():
     parser = argparse.ArgumentParser()
-    required = parser.add_argument_group('required arguments')
     parser.add_argument('-s', '--sessionid',help="Instagram session ID",required=True)
     parser.add_argument('-u','--username',help="One username",required=True)
     args = parser.parse_args()
+
     sessionsId=args.sessionid
 
+    infos = getInfo(args.username, sessionsId)
+    if not infos["user"]:
+        exit(infos["error"])
 
-    infos = getInfo(args.username,sessionsId)
-    if infos["user"]==None:
-        print(infos["error"])
-    else:
-        infos=infos["user"]
+    infos=infos["user"]
 
-        print("Informations about     : "+infos["username"])
-        print("Full Name              : "+infos["full_name"]+" | userID : "+infos["userID"])
-        print("Verified               : "+str(infos['is_verified'])+" | Is buisness Account : "+str(infos["is_business"]))
-        print("Is private Account     : "+str(infos["is_private"]))
-        print("Follower               : "+str(infos["follower_count"]) + " | Following : "+str(infos["following_count"]))
-        print("Number of posts        : "+str(infos["media_count"]))
-        print("Number of tag in posts : "+str(infos["following_tag_count"]))
+    print("Informations about     : "+infos["username"])
+    print("userID                 : "+infos["userID"])
+    print("Full Name              : "+infos["full_name"])
+    print("Verified               : "+str(infos['is_verified'])+" | Is buisness Account : "+str(infos["is_business"]))
+    print("Is private Account     : "+str(infos["is_private"]))
+    print("Follower               : "+str(infos["follower_count"]) + " | Following : "+str(infos["following_count"]))
+    print("Number of posts        : "+str(infos["media_count"]))
+    print("Number of tag in posts : "+str(infos["following_tag_count"]))
+    if infos["external_url"]:
         print("External url           : "+infos["external_url"])
-        print("IGTV posts             : "+str(infos["total_igtv_videos"]))
-        print("Biography              : "+infos["biography"])
-        if "public_email" in infos.keys():
-            if infos["public_email"]!='':
-                print("Public Email           : "+infos["public_email"])
-            else:
-                print("No public email found  : ")
-        if "public_phone_number"in infos.keys():
-            if str(infos["public_phone_number"])!='':
-                phonenr = "+"+str(infos["public_phone_country_code"])+" "+str(infos["public_phone_number"])
-                try:
-                    pn = phonenumbers.parse(phonenr)
-                    countrycode = region_code_for_country_code(pn.country_code)
-                    country = pycountry.countries.get(alpha_2=countrycode)
-                    phonenr = phonenr + " ({}) ".format(country.name)
-                except:
-                    pass
-                print("Public Phone number    : " + phonenr)
+    print("IGTV posts             : "+str(infos["total_igtv_videos"]))
+    print("Biography              : "+(f"""\n{" "*25}""").join(infos["biography"].split("\n")))
+    
+    if "public_email" in infos.keys():
+        if infos["public_email"]:
+            print("Public Email           : "+infos["public_email"])
 
-        other_infos=advanced_lookup(args.username)
-        if other_infos["error"]=="rate limit":
-            print("Rate limit please wait a few minutes before you try again")
-        elif "message" in other_infos["user"].keys():
-            if other_infos["user"]["message"]=="No users found":
-                print("The lookup did not work on this account")
-            else:
-                print("Rate limit please wait a few minutes before you try again.")
+    if "public_phone_number" in infos.keys():
+        if str(infos["public_phone_number"]):
+            phonenr = "+"+str(infos["public_phone_country_code"])+" "+str(infos["public_phone_number"])
+            try:
+                pn = phonenumbers.parse(phonenr)
+                countrycode = region_code_for_country_code(pn.country_code)
+                country = pycountry.countries.get(alpha_2=countrycode)
+                phonenr = phonenr + " ({}) ".format(country.name)
+            except: # except what ??
+                pass # pass what ??
+            print("Public Phone number    : " + phonenr)
+
+    other_infos=advanced_lookup(args.username)
+    
+    if other_infos["error"] == "rate limit":
+        print("Rate limit please wait a few minutes before you try again")
+    
+    elif "message" in other_infos["user"].keys():
+        if other_infos["user"]["message"] == "No users found":
+            print("The lookup did not work on this account")
         else:
-            if "obfuscated_email" in other_infos["user"].keys():
-                if other_infos["user"]["obfuscated_email"]!='':
-                    print("Obfuscated email       : "+other_infos["user"]["obfuscated_email"])
-                else:
-                    print("No obfuscated email found")
+            print(other_infos["user"]["message"])
+    
+    else:
+        if "obfuscated_email" in other_infos["user"].keys():
+            if other_infos["user"]["obfuscated_email"]:
+                print("Obfuscated email       : "+other_infos["user"]["obfuscated_email"])
+            else:
+                print("No obfuscated email found")
 
-            if "obfuscated_phone"in other_infos["user"].keys():
-                if str(other_infos["user"]["obfuscated_phone"])!='':
-                    print("Obfuscated phone       : "+str(other_infos["user"]["obfuscated_phone"]))
-                else:
-                    print("No obfuscated phone found")
-        print("-"*24)
-        print("Profile Picture        : "+infos["hd_profile_pic_url_info"]["url"])
+        if "obfuscated_phone"in other_infos["user"].keys():
+            if str(other_infos["user"]["obfuscated_phone"]):
+                print("Obfuscated phone       : "+str(other_infos["user"]["obfuscated_phone"]))
+            else:
+                print("No obfuscated phone found")
+    print("-"*24)
+    print("Profile Picture        : "+infos["hd_profile_pic_url_info"]["url"])
