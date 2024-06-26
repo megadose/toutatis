22a59c0984fd17d5e01cdb57669ec527b2c6b3d5
Palenath <megadose@protonmail.com>
2024-06-26 09:38:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='toutatis',
-    version="1.3",
+    version="1.31",
     packages=find_packages(),
     author="megadose",
     install_requires=["argparse","requests","phonenumbers","pycountry"],
